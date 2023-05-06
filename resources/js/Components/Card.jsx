fb984e4acf01801ce30db57f16a3cbe129b36cd0
Danni4421 <85418881+Danni4421@users.noreply.github.com>
2023-05-06 19:14:19 +0700

    feat: slicing dashboard

---
+++ resources/js/Components/Card.jsx
@@ -0,0 +1,35 @@
+import PropType from "prop-types";
+
+Card.propTypes = {
+    slug: PropType.string.isRequired,
+    name: PropType.string.isRequired,
+    description: PropType.string,
+    thumbnail: PropType.string.isRequired,
+};
+
+export default function Card({ slug, name, description, thumbnail }) {
+    return (
+        <div className="absolute group overflow-hidden mr-[30px]">
+            <img
+                src={thumbnail}
+                className="object-cover rounded-[30px] h-[340px] w-[250px]"
+                alt=""
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
+                <div className="px-7 pb-7">
+                    <div className="font-medium text-xl text-white">{name}</div>
+                    <p className="mb-0 text-gray-300 text-base mt-[10px]">
+                        {description}
+                    </p>
+                </div>
+            </div>
+            <div
+                className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
+                                -translate-x-1/2 z-20 transition ease-in-out duration-500"
+            >
+                <img src="/icons/ic_play.svg" className="" width="50" alt="" />
+            </div>
+            <a href={slug} className="inset-0 absolute z-50"></a>
+        </div>
+    );
+}
---
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -0,0 +1,67 @@
+import Flickity from "react-flickity-component";
+import { Head } from "@inertiajs/react";
+import Authenticated from "@/Pages/Auth/Authenticated/Index";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import { feature, browse } from "./../../listFilm.json";
+import Card from "@/Components/Card";
+
+export default function Dashboard() {
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+
+    return (
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+                <title>Dashboard</title>
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {feature.map((res) => {
+                        return (
+                            <FeaturedMovie
+                                key={res.key}
+                                slug={res.slug}
+                                name={res.name}
+                                category={res.category}
+                                thumbnail={`/images/featured-${res.imageKey}.png`}
+                                rating={res.rating}
+                            />
+                        );
+                    })}
+                </Flickity>
+            </div>
+            <div className="mt-10">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {browse.map((i) => {
+                        return (
+                            <Card
+                                key={i.key}
+                                slug={i.slug}
+                                name={i.name}
+                                description={i.desc}
+                                thumbnail={i.img}
+                            />
+                        );
+                    })}
+                </Flickity>
+            </div>
+        </Authenticated>
+    );
+}
